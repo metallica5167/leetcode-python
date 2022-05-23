fb9025b0968b09583838d755914237aeba1f1ef2
metallica5167 <pablo_99_yea@hotmail.com>
2022-05-23 00:18:28 -0400

    Organize code and add comments

--- 1523-count-odd-num.py
+++ 1523-count-odd-num.py
@@ -1,21 +1,15 @@
 #class Solution:
 #    def countOdds(self, low: int, high: int) -> int:
-high=7
-low=3
+#manually insert the numbers for testing without using function
 low=int(input("Insert low number: "))
 high=int(input("Insert high number: "))
 num=high-low
-add=low
-#count=num
+add=low #will be used to add all the numbers into the list
 numList=[]
 oddList=[]
 evenList=[]
-'''
-while num > low and num < high:
-    numList.append(num)
-    num+=1
-''' 
-for i in range(num+1):
+
+for i in range(num+1): #we add 1 so that it counts the high number in the cycle
     numList.append(add)
     add+=1
 
@@ -24,7 +18,10 @@ for i in range(len(numList)):
         oddList.append(numList[i])
     else:
         evenList.append(numList[i])
+
 print(oddList)
 print(len(oddList))
 print(evenList)
 print(len(evenList))
+
+#return len(oddList)
